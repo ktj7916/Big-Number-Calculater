dad14cbe5818199de03f6bb07f1ca1c4113e9492
ktj7916 <ktj7916@naver.com>
2016-05-31 15:55:46 +0900

    Update a.c
    필요한 기본코드 생성

--- a.c
+++ a.c
@@ -1,1 +1,31 @@
-;ljaldksfamwcw[oej[	
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+void plus(){
+  
+  
+}
+
+void minus(){
+  
+
+  
+}
+
+void multiply(){
+  
+  
+  
+}
+
+void divide(){
+  
+  
+  
+}
+int main ( ){
+  
+  
+}
+
